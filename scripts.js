90ffc2239a1fc51ea0d717f1da9a9e8c31f676b9
attocleaning <bnamo@uwo.ca>
2024-09-14 13:30:41 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,3 +11,10 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";  
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
+<script>
+    function toggleMenu() {
+        const navLinks = document.querySelector('.nav-links');
+        navLinks.classList.toggle('active');
+    }
+</script>
